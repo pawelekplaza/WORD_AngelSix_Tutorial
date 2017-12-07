7f658d4b6f9d4683c4a061a705a24a7d4cd4bb1c
Paweł Płaza <pawelekplaza@gmail.com>
2017-12-07 14:13:40 +0100

    Base project added.

---
+++ Word/Word/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Word
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
